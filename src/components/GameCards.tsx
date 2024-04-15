b31b76a3772906a15111fa55cb3549f4b3f0b694
Madan <madanmuniswamys@gmail.com>
2024-04-15 08:02:40 +0530

    Building Skeletons for GameCards

--- src/components/GameCards.tsx
+++ src/components/GameCards.tsx
@@ -10,7 +10,7 @@ interface Prop {
 }
 const GameCards = ({ game }: Prop) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card width="300px" borderRadius={10} overflow="hidden">
       <Image src={getCustomImage(game.background_image)} />
       <CardBody>
         <Heading fontSize="lg">{game.name}</Heading>
